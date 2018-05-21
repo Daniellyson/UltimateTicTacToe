f6416a0139a6adf963dd89b06c460739ceebd282
KJ1010G <38991076+KJ1010G@users.noreply.github.com>
2018-05-21 19:01:47 +0530

    Add files via upload

---
+++ Uttt.java
@@ -0,0 +1,21 @@
+class Uttt {
+  public static void main(String[] args) {
+    System.out.println("Welcome! To the game of Ultimate Tic Tac Toe");
+    boolean play = true;
+    while (play) {
+      System.out.println("1.Start a new game");
+      System.out.println("2.How to play");
+      System.out.println("3.Quit");
+      //get input
+      if () { // 1
+        // create game object, start
+      }
+      else if () { // 2
+        // print rules
+      }
+      else { // 3
+        play = false;
+      }
+    }
+  }
+}
